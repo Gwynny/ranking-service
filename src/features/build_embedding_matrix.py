b845fa17dca0defaf24cd4cc5bfc006172d0e0f3
Gwynny <yahoo660@gmail.com>
2023-02-02 12:38:03 +0300

    feat: add text retriever to process words
    This text retriever will used further to build embedding matrix.
    We will do lookup of retrieved words in pretrained GLOVE embeddings.
    Docstrings will be added further.

---
+++ src/features/build_embedding_matrix.py
@@ -0,0 +1,10 @@
+import itertools
+import nltk
+import os
+import string
+import torch
+import numpy as np
+import pandas as pd
+from collections import Counter
+from torch import nn
+from typing import Dict, List, Tuple
---
+++ src/features/text_retriever.py
@@ -0,0 +1,68 @@
+import itertools
+import nltk
+import string
+import pandas as pd
+from collections import Counter
+from typing import Dict, List
+
+
+class TextRetriever:
+    def __init__(self, train_path: str, val_path: str):
+        self.train_df = pd.read_csv(train_path, sep="\t")
+        self.val_df = pd.read_csv(val_path, sep="\t")
+
+    def _rename_cols_and_drop_na(self, quora_df: pd.DataFrame) -> pd.DataFrame:
+        quora_df = quora_df.dropna(axis=0, how="any").reset_index(drop=True)
+        quora_df_cols_renamed = pd.DataFrame(
+            {
+                "id_left": quora_df["qid1"],
+                "id_right": quora_df["qid2"],
+                "text_left": quora_df["question1"],
+                "text_right": quora_df["question2"],
+                "label": quora_df["is_duplicate"].astype(int),
+            }
+        )
+        return quora_df_cols_renamed
+
+    def _handle_punctuation(self, input_str: str) -> str:
+        translator = str.maketrans(string.punctuation,
+                                   " " * len(string.punctuation))
+        new_str = input_str.translate(translator)
+        return new_str
+
+    def _lower_and_tokenize_words(self, input_str: str) -> List[str]:
+        no_punctuation_str = self._handle_punctuation(input_str)
+        lowered_str = no_punctuation_str.lower()
+        splitted_doc = nltk.word_tokenize(lowered_str)
+        return splitted_doc
+
+    def _filter_rare_words(self,
+                           vocab: Dict[str, int],
+                           min_occurancies: int
+                           ) -> Dict[str, int]:
+        filtered_vocab = {
+            x: count for x, count in vocab.items() if count >= min_occurancies
+        }
+        return filtered_vocab
+
+    def get_all_tokens(self,
+                       list_of_df: List[pd.DataFrame],
+                       min_occurancies: int
+                       ) -> List[str]:
+        preped_series = []
+        for df in list_of_df:
+            df = self._rename_cols_and_drop_na(df)
+            preped_question1 = df["text_left"].apply(
+                                self._lower_and_tokenize_words)
+            preped_question2 = df["text_right"].apply(
+                                self._lower_and_tokenize_words)
+            preped_series.append(preped_question1)
+            preped_series.append(preped_question2)
+
+        concat_series = pd.concat(preped_series)
+        one_list_of_tokens = list(
+            itertools.chain.from_iterable(concat_series.to_list())
+        )
+        vocab = dict(Counter(one_list_of_tokens))
+        vocab = self._filter_rare_words(vocab, min_occurancies)
+        return list(vocab.keys())
