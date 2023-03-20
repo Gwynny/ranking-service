db1bad1f4527e2373b23cc1cd2484fdf3a8a8a48
Gwynny <yahoo660@gmail.com>
2023-03-20 12:09:50 +0300

    refactoring: train and val funcs as classmethods
    collate_fn, create_train_triplets and create_val_pairs are
    classmethods because they frequently used during training

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,7 +3,19 @@ import torch
 import numpy as np
 import pandas as pd
 from pandas.core.groupby.generic import DataFrameGroupBy
-from typing import Dict, List, Union, Callable
+from typing import Dict, List, Union, Callable, Tuple
+
+
+def prepare_quora_df(quora_df: pd.DataFrame) -> pd.DataFrame:
+    quora_df = quora_df.dropna(axis=0, how='any').reset_index(drop=True)
+    quora_df_fin = pd.DataFrame({
+        'id_left': quora_df['qid1'],
+        'id_right': quora_df['qid2'],
+        'text_left': quora_df['question1'],
+        'text_right': quora_df['question2'],
+        'label': quora_df['is_duplicate'].astype(int)
+    })
+    return quora_df_fin
 
 
 def get_idx_to_text_mapping(inp_df: pd.DataFrame) -> Dict[str, str]:
@@ -25,86 +37,9 @@ def get_idx_to_text_mapping(inp_df: pd.DataFrame) -> Dict[str, str]:
     return left_dict
 
 
-def get_question_groups(inp_df: pd.DataFrame,
-                        min_group_size: int = 3) -> DataFrameGroupBy:
-    inp_df_select = inp_df[['id_left', 'id_right', 'label']]
-    inf_df_group_sizes = inp_df_select.groupby('id_left').size()
-    leftids_to_use = list(
-        inf_df_group_sizes[inf_df_group_sizes >= min_group_size].index
-    )
-    groups = inp_df_select[
-                inp_df_select['id_left'].isin(leftids_to_use)
-             ].groupby('id_left')
-    return groups
-
-
-def sample_data_for_train_iter(inp_df: pd.DataFrame,
-                               seed: int,
-                               min_group_size: int = 3,
-                               num_positive_examples: int = 4,
-                               num_same_rel_examples: int = 2
-                               ) -> List[List[Union[str, float]]]:
-    np.random.seed(seed)
-    groups = get_question_groups(inp_df, min_group_size)
-    out_triplets = []
-    for id_left, group in groups:
-        if group['label'].sum() == 0:
-            continue
-        ones_df = group[group['label'] == 1]
-        zeros_df = group[group['label'] == 0]
-
-        if len(zeros_df) > 1:
-            ones_ids = ones_df['id_right'].to_list()
-            np.random.shuffle(ones_ids)
-            zeros_ids = zeros_df['id_right'].to_list()
-            pos_labels_permutations = [
-                (one_id, zero_id) for one_id in ones_ids for zero_id in zeros_ids]
-            np.random.shuffle(pos_labels_permutations)
-            for ids in pos_labels_permutations[:num_positive_examples]:
-                out_triplets.append([id_left, ids[0], ids[1], 1.0])
-
-            zeros_permutations = list(itertools.combinations(zeros_ids, 2))
-            np.random.shuffle(zeros_permutations)
-            for ids in zeros_permutations[:num_same_rel_examples]:
-                out_triplets.append([id_left, ids[0], ids[1], 0.5])
-    return out_triplets
-
-
-def create_val_pairs(inp_df: pd.DataFrame,
-                     fill_top_to: int = 15,
-                     min_group_size: int = 2,
-                     seed: int = 0
-                     ) -> List[List[Union[str, float]]]:
-    np.random.seed(seed)
-    groups = get_question_groups(inp_df, min_group_size)
-
-    all_ids = set(inp_df['id_left']).union(set(inp_df['id_right']))
-    out_pairs = []
-    for id_left, group in groups:
-        ones_ids = group[group.label > 0].id_right.values
-        zeroes_ids = group[group.label == 0].id_right.values
-        sum_len = len(ones_ids) + len(zeroes_ids)
-        num_pad_items = max(0, fill_top_to - sum_len)
-
-        if num_pad_items > 0:
-            cur_chosen = set(ones_ids).union(
-                set(zeroes_ids)).union({id_left})
-            pad_sample = np.random.choice(
-                list(all_ids - cur_chosen), num_pad_items,
-                replace=False).tolist()
-        else:
-            pad_sample = []
-
-        for i in ones_ids:
-            out_pairs.append([id_left, i, 2])
-        for i in zeroes_ids:
-            out_pairs.append([id_left, i, 1])
-        for i in pad_sample:
-            out_pairs.append([id_left, i, 0])
-    return out_pairs
-
-
 class RankingDataset(torch.utils.data.Dataset):
+    OUT_DICT = Dict[str, torch.LongTensor]
+
     def __init__(self, index_pairs_or_triplets: List[List[Union[str, float]]],
                  idx_to_text_mapping: Dict[int, str],
                  vocab: Dict[str, int],
@@ -117,6 +52,77 @@ class RankingDataset(torch.utils.data.Dataset):
         self.oov_val = oov_val
         self.preproc_func = preproc_func
         self.max_len = max_len
+
+    @classmethod
+    def collate_fn(cls,
+                   batch_objs: List[Union[Dict[str, torch.Tensor], torch.FloatTensor]]
+                   ) -> Union[Tuple[OUT_DICT, OUT_DICT, torch.FloatTensor],
+                              Tuple[OUT_DICT, torch.FloatTensor]]:
+        max_len_q1, max_len_d1 = -1, -1
+        max_len_q2, max_len_d2 = -1, -1
+
+        is_triplets = False
+        for elem in batch_objs:
+            if len(elem) == 3:
+                left_elem, right_elem, label = elem
+                is_triplets = True
+            else:
+                left_elem, label = elem
+
+            max_len_q1 = max(len(left_elem['query']), max_len_q1)
+            max_len_d1 = max(len(left_elem['document']), max_len_d1)
+            if len(elem) == 3:
+                max_len_q2 = max(len(right_elem['query']), max_len_q2)
+                max_len_d2 = max(len(right_elem['document']), max_len_d2)
+
+        q1s, d1s = [], []
+        q2s, d2s = [], []
+        labels = []
+        for elem in batch_objs:
+            if is_triplets:
+                left_elem, right_elem, label = elem
+            else:
+                left_elem, label = elem
+
+            pad_len1 = max_len_q1 - len(left_elem['query'])
+            pad_len2 = max_len_d1 - len(left_elem['document'])
+            if is_triplets:
+                pad_len3 = max_len_q2 - len(right_elem['query'])
+                pad_len4 = max_len_d2 - len(right_elem['document'])
+
+            q1s.append(left_elem['query'] + [0] * pad_len1)
+            d1s.append(left_elem['document'] + [0] * pad_len2)
+            if is_triplets:
+                q2s.append(right_elem['query'] + [0] * pad_len3)
+                d2s.append(right_elem['document'] + [0] * pad_len4)
+            labels.append([float(label)])
+
+        q1s, d1s = torch.LongTensor(q1s), torch.LongTensor(d1s)
+        if is_triplets:
+            q2s, d2s = torch.LongTensor(q2s), torch.LongTensor(d2s)
+        labels = torch.FloatTensor(labels)
+
+        ret_left = {'query': q1s, 'document': d1s}
+        if is_triplets:
+            ret_right = {'query': q2s, 'document': d2s}
+            return ret_left, ret_right, labels
+        else:
+            return ret_left, labels
+    
+    @classmethod
+    def get_question_groups(cls,
+                            inp_df: pd.DataFrame,
+                            min_group_size: int = 3
+                            ) -> DataFrameGroupBy:
+        inp_df_select = inp_df[['id_left', 'id_right', 'label']]
+        inf_df_group_sizes = inp_df_select.groupby('id_left').size()
+        leftids_to_use = list(
+            inf_df_group_sizes[inf_df_group_sizes >= min_group_size].index
+        )
+        groups = inp_df_select[
+                    inp_df_select['id_left'].isin(leftids_to_use)
+                ].groupby('id_left')
+        return groups
 
     def __len__(self):
         return len(self.index_pairs_or_triplets)
@@ -139,7 +145,7 @@ class RankingDataset(torch.utils.data.Dataset):
 
 
 class TrainTripletsDataset(RankingDataset):
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         triplets = self.index_pairs_or_triplets[idx]
         query_tokens = self._convert_text_idx_to_token_idxs(str(triplets[0]))
         left_doc_tokens = self._convert_text_idx_to_token_idxs(str(triplets[1]))
@@ -150,12 +156,80 @@ class TrainTripletsDataset(RankingDataset):
         right_query_doc = {'query': query_tokens, 'document': right_doc_tokens}
         return left_query_doc, right_query_doc, label
 
+    @classmethod
+    def create_train_triplets(cls,
+                              inp_df: pd.DataFrame,
+                              seed: int,
+                              min_group_size: int = 3,
+                              num_positive_examples: int = 4,
+                              num_same_rel_examples: int = 2
+                              ) -> List[List[Union[str, float]]]:
+        np.random.seed(seed)
+        groups = RankingDataset.get_question_groups(inp_df, min_group_size)
+        out_triplets = []
+        for id_left, group in groups:
+            if group['label'].sum() == 0:
+                continue
+            ones_df = group[group['label'] == 1]
+            zeros_df = group[group['label'] == 0]
+
+            if len(zeros_df) > 1:
+                ones_ids = ones_df['id_right'].to_list()
+                np.random.shuffle(ones_ids)
+                zeros_ids = zeros_df['id_right'].to_list()
+                pos_labels_permutations = [
+                    (one_id, zero_id) for one_id in ones_ids for zero_id in zeros_ids]
+                np.random.shuffle(pos_labels_permutations)
+                for ids in pos_labels_permutations[:num_positive_examples]:
+                    out_triplets.append([id_left, ids[0], ids[1], 1.0])
+
+                zeros_permutations = list(itertools.combinations(zeros_ids, 2))
+                np.random.shuffle(zeros_permutations)
+                for ids in zeros_permutations[:num_same_rel_examples]:
+                    out_triplets.append([id_left, ids[0], ids[1], 0.5])
+        return out_triplets
+
 
 class ValPairsDataset(RankingDataset):
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         pairs = self.index_pairs_or_triplets[idx]
         query_tokens = self._convert_text_idx_to_token_idxs(str(pairs[0]))
         doc_tokens = self._convert_text_idx_to_token_idxs(str(pairs[1]))
         label = pairs[2]
         query_doc = {'query': query_tokens, 'document': doc_tokens}
         return query_doc, label
+
+    @classmethod
+    def create_val_pairs(cls,
+                         inp_df: pd.DataFrame,
+                         fill_top_to: int = 15,
+                         min_group_size: int = 2,
+                         seed: int = 0
+                         ) -> List[List[Union[str, float]]]:
+        np.random.seed(seed)
+        groups = RankingDataset.get_question_groups(inp_df, min_group_size)
+
+        all_ids = set(inp_df['id_left']).union(set(inp_df['id_right']))
+        out_pairs = []
+        for id_left, group in groups:
+            ones_ids = group[group.label > 0].id_right.values
+            zeroes_ids = group[group.label == 0].id_right.values
+            sum_len = len(ones_ids) + len(zeroes_ids)
+            num_pad_items = max(0, fill_top_to - sum_len)
+
+            if num_pad_items > 0:
+                cur_chosen = set(ones_ids).union(
+                    set(zeroes_ids)).union({id_left})
+                pad_sample = np.random.choice(
+                    list(all_ids - cur_chosen), num_pad_items,
+                    replace=False).tolist()
+            else:
+                pad_sample = []
+
+            for i in ones_ids:
+                out_pairs.append([id_left, i, 2])
+            for i in zeroes_ids:
+                out_pairs.append([id_left, i, 1])
+            for i in pad_sample:
+                out_pairs.append([id_left, i, 0])
+        return out_pairs
